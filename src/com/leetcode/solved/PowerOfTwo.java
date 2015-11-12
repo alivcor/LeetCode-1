9a85773688f0fb9c17376a196c17270fc2e8bc9f
AmanRaj <amankaraj@gmail.com>
2015-11-12 18:03:39 -0500

    Power of Two

---
+++ src/com/leetcode/solved/PowerOfTwo.java
@@ -0,0 +1,20 @@
+package com.leetcode.solved;
+
+public class PowerOfTwo {
+
+	/*
+	 * If number is zero or negative, it would not be a power of 2.
+	 * For others we know if there is just one set bit in the integer, it is a power of 2.
+	 */
+	public static boolean isPowerOfTwo(int n) {
+		if(n <= 0) return false;
+		return ((n&(n-1)) == 0) ? true : false;
+    }
+	public static void main(String[] args) {
+		System.out.println(isPowerOfTwo(8));
+		System.out.println(isPowerOfTwo(1023));
+		System.out.println(isPowerOfTwo(1024));
+
+	}
+
+}
