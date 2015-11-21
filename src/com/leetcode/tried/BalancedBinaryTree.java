ae75085cd52441842002e5080c655374211c0eec
AmanRaj <amankaraj@gmail.com>
2015-11-21 17:09:36 -0500

    Balanced Binary Tree

--- src/com/leetcode/tried/BalancedBinaryTree.java
+++ src/com/leetcode/tried/BalancedBinaryTree.java
@@ -6,16 +6,42 @@ public class BalancedBinaryTree {
 		      TreeNode left;
 		      TreeNode right;
 		      TreeNode(int x) { val = x; }
+			@Override
+			public String toString() {
+				return "TreeNode [val=" + val + ", left=" + left + ", right="
+						+ right + "]";
+			}
+		      
 		  };
 
-	  public boolean isBalanced(TreeNode root) {
-	     if(root == null || (root.left == null && root.right == null)) return true;
-	     if(root.left == null || root.right == null) return false;
-	     
-	     return (isBalanced(root.left) && isBalanced(root.right)); 
-	    }
+	  public static int[] isBal(TreeNode root){
+			if(root == null)  return new int[]{1,0}; 
+			if((root.left == null && root.right == null)) return new int[]{1,1};
+			
+			int[] LArray = isBal(root.left);
+			int[] RArray = isBal(root.right);
+			
+			if(LArray[0] == 0 || RArray[0] == 0){
+				return new int[]{0,1};
+			}
+			
+			if(Integer.max(LArray[1],RArray[1]) - Integer.min(LArray[1],RArray[1]) <= 1)
+				return new int[]{1,Integer.max(LArray[1], RArray[1]) + 1};
+			
+			return new int[]{0,0};
+		}
+		
+	public static boolean isBalanced(TreeNode root) {
+		int[] arr =  isBal(root); 
+	    return (arr[0] == 0) ? false : true;
+	}
+	  
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		TreeNode root = new TreeNode(1);
+		root.right = new TreeNode(2);
+		root.right.right = new TreeNode(3);
+		
+		System.out.println(isBalanced(root));
 
 	}
 
