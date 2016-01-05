ca9fe2ce82847b68b071c8bfb88ea2cc0683ebb8
AmanRaj <amankaraj@gmail.com>
2016-01-05 14:46:08 -0500

    https://leetcode.com/problems/reverse-linked-list/

--- src/com/leetcode/solved/ReverseLinkList.java
+++ src/com/leetcode/solved/ReverseLinkList.java
@@ -7,7 +7,22 @@ public class ReverseLinkList {
 	      ListNode(int x) { val = x; }
 	  }
 	
-	
+	/*
+	 * If head is null or a single node list = ReverseList == List.
+	 * Else:
+	 * 1 -> 2 -> 3 -> 4. to 4 -> 3 -> 2 -> 1
+	 * 
+	 * Steps:
+	 * Keep the head aside and recurse on rest. In this case that would return:
+	 * 4 -> 3 -> 2. At this point of time, we need to attach head to end of list.
+	 * So before calling the recursive function, save the pointer to 2(rest).
+	 * 
+	 * After recursion point the next pointer of rest to head. (2 -> 1).
+	 * At the same time free the next pointer of head by assigning it to null.
+	 * So the final list is :
+	 * 4 -> 3 -> 2 -> 1.
+	 * Return the list that we got from recursive function.
+	 */
     public ListNode reverseList(ListNode head) {
     
     	if(head == null || head.next == null) return head;
