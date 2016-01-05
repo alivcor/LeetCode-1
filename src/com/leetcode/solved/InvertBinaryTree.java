363e399c010b4ca5812e191c76efb1a1328ea9c5
AmanRaj <amankaraj@gmail.com>
2016-01-04 22:01:23 -0500

    https://leetcode.com/problems/invert-binary-tree/

--- src/com/leetcode/solved/InvertBinaryTree.java
+++ src/com/leetcode/solved/InvertBinaryTree.java
@@ -7,19 +7,25 @@ public class InvertBinaryTree {
 		int val;
 		TreeNode left, right, next;
 	}
+	/*
+	 * Check if the root is null, return null. There is no tree.
+	 * If the root is not null, for all the non-leaf nodes, 
+	 * swap left and right children.
+	 * 
+	 * In the code, 
+	 * We will invert left and right subtree. Once done,
+	 * We will swap left and right pointer to the original tree. 
+	 */
 	public TreeNode invertTree(TreeNode root) {
+        if(root == null) return root;
+        invertTree(root.left);
+        invertTree(root.right);
         
-		if(root == null) return null;
-		if(root.left == null && root.right == null) return root;
-		
-		TreeNode temp = (root.left != null) ? root.left : null;
-		root.left = root.right;
-		root.right = temp;
-		
-		invertTree(root.left);
-		invertTree(root.right);
-		
-		return root;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        return root;
     }
 
 }
