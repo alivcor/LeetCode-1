9466e64e6948c16fdf0a0b8c4512233d6fd303ef
AmanRaj <amankaraj@gmail.com>
2016-01-05 12:10:11 -0500

    https://leetcode.com/problems/excel-sheet-column-number/

--- src/com/leetcode/solved/ExcelSheetColumnNumber.java
+++ src/com/leetcode/solved/ExcelSheetColumnNumber.java
@@ -2,28 +2,22 @@ package com.leetcode.solved;
 
 public class ExcelSheetColumnNumber {
 
-	private static int power(int val, int pow){
-		int prod = 1;
-		while(pow > 0){
-			prod = prod * val;
-			pow--;
-		}
-		
-		return prod;
-	}
-	
+	/*
+	 * Given a column title calculate column#. Starting with A = 1.
+	 * Define two variables colNumber = to store the intermediate column numbers
+	 * currentMul = Multiplicity factor.
+	 * 
+	 * Loop through the array, adding to the column number current char multiplied by multiplicity factor.
+	 */
 	public static int titleToNumber(String s) {
-        
-        if(s == null || s.length() == 0) return 0;
-        
-        if(s.length() == 1) {
-        	if(s.charAt(0) >= 'A' && s.charAt(0) <= 'Z'){
-        		return s.charAt(0) - 'A' + 1;
-        	}
+        int colNumber = 0;
+        int currentMul = 1;
+        for(int i = s.length()-1; i>=0; i--){
+            colNumber += currentMul*(s.charAt(i) - 'A' + 1);
+            currentMul *= 26;
         }
         
-        return (power(26, s.length()-1)*(s.charAt(0) - 'A' + 1) + titleToNumber(s.substring(1))); 
-        
+        return colNumber;
     }
 	public static void main(String[] args) {
 		System.out.println(titleToNumber("AB"));
