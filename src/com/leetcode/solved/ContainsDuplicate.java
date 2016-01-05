4516995637a56ee4e72070989b86281fb0d82564
AmanRaj <amankaraj@gmail.com>
2016-01-05 11:52:32 -0500

    https://leetcode.com/problems/contains-duplicate/

---
+++ src/com/leetcode/solved/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+package com.leetcode.solved;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+	/*
+	 * Return true if there is at least one duplicate in the entire array.
+	 * 
+	 * If the array is null/ length 0 or 1, it cannot contain any duplicates.
+	 * Create a set of Integers, traverse through the array, 
+	 * If the current element is already present, return true.
+	 * If we have traversed through the complete array and none of the elements repeat.
+	 * Return false.
+	 */
+	public boolean containsDuplicate(int[] nums) {
+        
+        if(nums == null || nums.length == 0 || nums.length == 1) return false;
+        
+        HashSet<Integer> setInt = new HashSet<Integer>();
+        for(int i : nums){
+            if(setInt.contains(i))
+                return true;
+            setInt.add(i);
+        }
+        
+        return false;
+    }
+}
