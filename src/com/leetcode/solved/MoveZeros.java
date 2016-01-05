b42b907e9ca8bb01722347eac682514b31ca4a17
AmanRaj <amankaraj@gmail.com>
2016-01-04 21:25:53 -0500

    https://leetcode.com/problems/move-zeroes/

--- src/com/leetcode/solved/MoveZeros.java
+++ src/com/leetcode/solved/MoveZeros.java
@@ -2,9 +2,16 @@ package com.leetcode.solved;
 
 
 public class MoveZeros {
-	
+	/*
+	 * Move all zeros to end of array maintaining relative order of elements.
+	 * 
+	 *  Maintain two indices.
+	 *  Move indices and as soon as we encounter a zero, overwrite.
+	 *  Write rest of the array as zero.
+	 *
+	 */
 	public void moveZeroes(int[] nums) {
-	        
+	        if(nums == null || nums.length == 0) return;
 	        int index = 0;
 	        int indexNZ = 0;
 	        
