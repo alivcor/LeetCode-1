100ebec85644f9b93ef39132515a40dd7e5c7af7
AmanRaj <amankaraj@gmail.com>
2016-01-05 20:25:39 -0500

    https://leetcode.com/problems/happy-number/

--- src/com/leetcode/solved/HappyNumber.java
+++ src/com/leetcode/solved/HappyNumber.java
@@ -4,7 +4,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HappyNumber {
-	
+	/*
+	 * To prevent the number from looping in a cycle, 
+	 * We will store all the square sum in a set, return false if repeat.
+	 * 
+	 * We will run the loop until we have n == 1.
+	 * Any cycle would anyway return false.
+	 */
 	public static boolean isHappy(int n) {
         Set<Integer> setPrevious = new HashSet<Integer>();
         while(n != 1){
