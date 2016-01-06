21e7a50572bd3e45597bfe8dfacca677320593e2
AmanRaj <amankaraj@gmail.com>
2016-01-05 20:21:57 -0500

    https://leetcode.com/problems/maximum-product-of-word-lengths/

--- src/com/leetcode/solved/MaxProdWordLength.java
+++ src/com/leetcode/solved/MaxProdWordLength.java
@@ -1,7 +1,13 @@
 package com.leetcode.solved;
 
 public class MaxProdWordLength {
-
+	
+	/*
+	 * The function which maps each String to an integer.
+	 * This function would set few bits corrosponding to the respective characters.
+	 * 
+	 * This would help us determine if two strings have distinct set of characters in O(1) time.
+	 */
 	private static int mapString(String s){
 		int intVal = 0;
 		for(int i = 0; i < s.length(); i++){
@@ -10,6 +16,14 @@ public class MaxProdWordLength {
 		
 		return intVal;
 	}
+	/*
+	 * Create Integer value for each of the characters.
+	 * Now traverse the array of strings, comparing each word with every other word.
+	 * If the characters are distinct and the product of these two form maxProd.
+	 * Replace.
+	 * 
+	 * Return maxProd
+	 */
 	public static int maxProduct(String[] words) {
         int[] valString = new int[words.length];
         
