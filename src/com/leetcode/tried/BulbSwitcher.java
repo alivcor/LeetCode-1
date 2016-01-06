313cfd9b3f4d11ac17f4503c24ea4b1486b9f695
AmanRaj <amankaraj@gmail.com>
2016-01-05 21:31:48 -0500

    https://leetcode.com/problems/bulb-switcher/

---
+++ src/com/leetcode/tried/BulbSwitcher.java
@@ -0,0 +1,23 @@
+package com.leetcode.tried;
+
+public class BulbSwitcher {
+
+	/*
+	 * For any number N, it can be represented as p * q.
+	 * So, for any bulb B_n it would be switched on by p and off by q.
+	 * Basically it will remain in the same state.
+	 * 
+	 * All numbers except squares: As along with all pair of factors,
+	 * A square can be represented as p*p. So toggled only once.
+	 * 
+	 * So, all the bulbs at positions which are square of some number would remain on.
+	 */
+	public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
