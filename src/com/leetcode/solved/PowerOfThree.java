22027be8a3d6859093189f2cfe9113ad9f363820
AmanRaj <amankaraj@gmail.com>
2016-01-15 23:59:36 -0500

    https://leetcode.com/problems/power-of-three/

--- src/com/leetcode/solved/PowerOfThree.java
+++ src/com/leetcode/solved/PowerOfThree.java
@@ -1,11 +1,16 @@
+
 package com.leetcode.solved;
 
 public class PowerOfThree {
 	
+	/**This is valid for constant time checking of any power.
+	 * If the log value is an integer, the number is a power of given.
+	 * @param n
+	 * @return
+	 */
 	public static boolean isPowerOfThree(int n) {
 		if(n <= 0) return false;
-		double logbase3 = Math.log(n)/Math.log(3);
-		logbase3 = Math.round(logbase3*1000000000)/1000000000.00;
+		double logbase3 = Math.log10(n)/Math.log10(3);
 		return (logbase3%1 == 0);  
     }
 
