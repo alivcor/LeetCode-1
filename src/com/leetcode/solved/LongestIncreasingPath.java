1a3a3a35be1f3b6ad2926bbac6957f1e44716550
AmanRaj <amankaraj@gmail.com>
2016-02-01 22:15:04 -0500

    https://leetcode.com/problems/longest-increasing-path-in-a-matrix/

---
+++ src/com/leetcode/solved/LongestIncreasingPath.java
@@ -0,0 +1,48 @@
+package com.leetcode.solved;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestIncreasingPath {
+	static Map<String,Integer> dpMap = new HashMap<String,Integer>();
+    public static int longestIncreasingPath(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int maxLength = 0;
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                maxLength = Integer.max(maxLength, maxLength(matrix,i,j,matrix[i][j]-1));
+            }
+        }
+        return maxLength;
+    }
+    
+    private static int retMax(int a, int b, int c, int d){
+        return (Integer.max(Integer.max(a,b), Integer.max(c,d)));
+    }
+    private static int maxLength(int[][] matrix, int x, int y, int val){
+        
+        if(x < 0 || x == matrix.length || y < 0 || y == matrix[0].length) return 0;
+        if(matrix[x][y] <= val) return 0;
+        if(dpMap.get(x + "_" + y) != null) return dpMap.get(x + "_" + y);
+        
+        int temp = matrix[x][y];
+        matrix[x][y] = Integer.MIN_VALUE;
+        int result =  retMax(
+            maxLength(matrix,x+1,y,temp),
+            maxLength(matrix,x-1,y,temp),
+            maxLength(matrix,x,y+1,temp),
+            maxLength(matrix,x,y-1,temp)
+            );
+        matrix[x][y] = temp;
+        dpMap.put(x + "_" + y,1 + result); 
+        return 1 + result;
+    }
+
+
+	public static void main(String[] args) {
+		int[][] matrix = {{9,9,4},{6,6,8},{2,1,1}};
+		System.out.println(longestIncreasingPath(matrix ));
+		System.out.println(dpMap);
+	}
+
+}
