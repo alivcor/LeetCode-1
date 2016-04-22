7b806b8db668532c6c1f28d2c7a9c57e6ddbc45a
AmanRaj <amankaraj@gmail.com>
2016-04-22 15:50:02 -0400

    https://leetcode.com/problems/merge-k-sorted-lists/

--- src/com/leetcode/solved/MergeKSortedList.java
+++ src/com/leetcode/solved/MergeKSortedList.java
@@ -32,4 +32,29 @@ public class MergeKSortedList {
         }    
         return head.next;
     }
+	
+	public ListNode mergeKLists1(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        return mergeKLists(lists,0,lists.length-1);
+    }
+	
+	public ListNode merge(ListNode l1, ListNode l2){
+		if(l1 == null) return l2;
+		if(l2 == null) return l1;
+		
+		if(l1.val < l2.val) {
+			l1.next = merge(l1.next, l2);
+			return l1;
+		}else{
+			l2.next = merge(l1, l2.next);
+			return l2;
+		}
+	}
+	public ListNode mergeKLists(ListNode[] lists, int start, int end) {
+        if(start > end) return null;
+        if(start == end) return lists[start];
+        if(end - start == 1) return merge(lists[start],lists[end]);
+        int mid = (start + end)/2;
+        return merge(mergeKLists(lists, start, mid), mergeKLists(lists, mid+1, end));
+    }
 }
