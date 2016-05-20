06d191e50314240cf3964e211caa369168d13712
AmanRaj <amankaraj@gmail.com>
2016-05-20 18:02:37 -0400

    https://leetcode.com/problems/gas-station/

--- src/com/leetcode/solved/GasStation.java
+++ src/com/leetcode/solved/GasStation.java
@@ -1,10 +1,19 @@
 package com.leetcode.solved;
 
 public class GasStation {
+/*
+If car starts at A and can not reach B. Any station between A and B can not reach B.(B is the first station that A can not reach.)
+Proof to the first point: say there is a point C between A and B -- that is A can reach C but cannot reach B.
+Since A cannot reach B, the gas collected between A and B is short of the cost.
+Starting from A, at the time when the car reaches C, it brings in gas >= 0, and the car still cannot reach B. 
+Thus if the car just starts from C, it definitely cannot reach B.
 
+
+If the total number of gas is bigger than the total number of cost. There must be a solution.
+ 
+ */
 	public static int canCompleteCircuit(int[] gas, int[] cost) {
         if(gas == null || cost == null) return -1;
-        if(gas.length == 1) return (gas[0] >= cost[0]) ? 0 : -1;
         
         int totalGas = 0;
         int totalCost = 0;
