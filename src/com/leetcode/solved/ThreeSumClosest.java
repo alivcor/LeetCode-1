bacd3190b0433f4214bf1f1948f101fc910cca6e
AmanRaj <amankaraj@gmail.com>
2016-05-21 12:10:49 -0400

    https://leetcode.com/problems/3sum-closest/

---
+++ src/com/leetcode/solved/ThreeSumClosest.java
@@ -0,0 +1,36 @@
+package com.leetcode.solved;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ThreeSumClosest {
+
+	public static int  threeSumClosest(int[] nums, int target) {
+		if(nums == null) return 0;
+		if(nums.length < 3) return Arrays.stream(nums).sum();
+		int closest = nums[0] + nums[1] + nums[2];
+		
+		Arrays.sort(nums);
+		for(int i = 0; i < nums.length-2; i++){
+			int j = i + 1;
+			int k = nums.length - 1;
+			while(j < k){
+				int currSum = nums[i] + nums[j] + nums[k];
+				if(Math.abs(target - currSum) < Math.abs(closest - target)) closest = currSum;
+				if(currSum == target) return currSum;
+				else if(currSum < target) j++;
+				else k--;
+			}
+		}
+	    return closest;
+	}
+	
+	public static void main(String[] args) {
+		int nums[] = {0,2,1,-3};
+		System.out.println(threeSumClosest(nums, 1));
+
+	}
+
+}
