2b8f403ae7ad6ac8a8b0598b61e5347f74b6f11a
AmanRaj <amankaraj@gmail.com>
2016-05-27 19:13:05 -0400

    https://leetcode.com/problems/zigzag-conversion/

---
+++ src/com/leetcode/solved/ZigZagConversion.java
@@ -0,0 +1,34 @@
+package com.leetcode.tried;
+
+import java.util.stream.IntStream;
+
+public class ZigZagConversion {
+	
+	public static String convert(String s, int numRows) {
+		if(numRows <= 1) return s;
+		StringBuilder[] sbArray = new StringBuilder[numRows];
+		for(int i = 0; i < sbArray.length; i++) sbArray[i] = new StringBuilder();
+		
+		int index = 0;
+		while(index < s.length()){
+			//Zig
+			for(int i = 0; i < numRows && index < s.length(); i++){
+				sbArray[i].append(s.charAt(index++));
+			}
+			//Zag
+			for(int i = numRows-2; i > 0 && index < s.length(); i--){
+				sbArray[i].append(s.charAt(index++));
+			}
+		}
+		
+		IntStream.range(1, sbArray.length).forEach(i -> sbArray[0].append(sbArray[i]));
+		return sbArray[0].toString();
+    }
+
+	public static void main(String[] args) {
+		System.out.println(convert("PAYPALISHIRING", 3));
+		System.out.println(convert("ABCDE", 4));
+
+	}
+
+}
