00c9441ff22453c4d7b93cd0220d57098c09b46e
AmanRaj <amankaraj@gmail.com>
2016-05-31 15:20:25 -0400

    https://leetcode.com/problems/distinct-subsequences/

--- src/com/leetcode/solved/DistinctSubsequences.java
+++ src/com/leetcode/solved/DistinctSubsequences.java
@@ -1,76 +1,48 @@
-package com.leetcode.tried;
+package com.leetcode.solved;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class DistinctSubsequences {
 
-	
-	public static int numDistinctRecur(String s, String t) {
-		
-		if(s.length() < t.length()) return 0;
-		if(t.length() == 0) return 1;
-		
-		if(s.charAt(0) == t.charAt(0)){
-			return (numDistinctRecur(s.substring(1), t.substring(1)) + numDistinctRecur(s.substring(1), t));
-		}
-		
-		return numDistinctRecur(s.substring(1), t);
-		
+	Map<String, Integer> dpmp = new HashMap<>();
+	public int numDistinct(String s,String t) {
+		dpmp = new HashMap<>();
+		if(s == null || t == null) return 0;
+		return numDistUtil(s, 0, t, 0);
     }
 	
-	static HashMap<String, Integer> data = new HashMap<>();
-	public static int numDistinct(String s, String t){
-		if(s.length() < t.length()) return 0;
-		return numDistinct(s, t, 0, 0);
+	private  int numDistUtil(String s, int is, String t, int it){
+		if(it == t.length()) return 1;
+		if(s.length() - is < t.length() - it) return 0;
+		
+		String s_t = is + " " + it;
+		if(dpmp.containsKey(s_t)) return dpmp.get(s_t);
+		if(s.charAt(is) == t.charAt(it)) dpmp.put(s_t, numDistUtil(s, is+1, t, it+1) + numDistUtil(s,is+1,t,it));
+		else dpmp.put(s_t, numDistUtil(s,is+1,t,it));
+		
+		return dpmp.get(s_t);
 	}
-	public static int numDistinct(String s, String t, int sindex, int tindex){
-		
-		if(sindex < tindex) return 0;
-		if(tindex == t.length()) return 1;
-
-		if(s.length() == sindex) return 0;
-		
-		
-		String s1t1 = (sindex+1) + " " + (tindex+1);
-		String s1t0 = (sindex+1) +" "+ tindex;
-		
-		if(!data.containsKey(s1t1))
-			data.put(s1t1,numDistinct(s, t,sindex+1,tindex+1));
-		
-		if(!data.containsKey(s1t0))
-			data.put(s1t0,numDistinct(s, t,sindex+1,tindex));
-		
-		if(s.charAt(sindex) == t.charAt(tindex)){
-			return data.get(s1t1) + data.get(s1t0);
-		}
-		
-		return  data.get(s1t0);
-		
-	}
-	public static int numDistinctDP(String s, String t){
-		
-		if(s.length() < t.length()) return 0;
-		int data[][] = new int[s.length()+1][t.length()+1];
-		
-		for(int i = 0; i <= s.length(); i++)
-			data[i][0] = 1;
-		
+	
+	public int numDistinctItr(String s, String t){
+		if(s == null || t == null) return 0;
+		int dpArray[][] = new int[s.length() + 1][t.length() + 1];
+		for(int i = 0; i <= s.length(); i++) dpArray[i][0] = 1;
 		
 		for(int i = 1; i <= s.length(); i++){
-			for(int j = i; j <= t.length(); j++){
-				if(s.charAt(i-1) == s.charAt(j-1)){
-					data[i][j] = data[i-1][j] + data[i-1][j];
-				}else{
-					data[i][j] = data[i-1][j-1];
-				}
+			for(int j = 1; j <= t.length(); j++){
+				if(s.charAt(i-1) == t.charAt(j-1)){
+					dpArray[i][j] = dpArray[i-1][j-1] + dpArray[i-1][j];
+				}else dpArray[i][j] = dpArray[i-1][j];
 			}
 		}
-		
-		return data[s.length()][t.length()];
+		return dpArray[s.length()][t.length()];
 	}
 	public static void main(String[] args) {
-		System.out.println(numDistinct("ccc", "c"));
-		//System.out.println(numDistinctDP("abbbi", "abbi"));
+		//System.out.println(numDistinct("", ""));
+		DistinctSubsequences ds = new DistinctSubsequences();
+		System.out.println(ds.numDistinctItr("ccccss", "cs"));
+		System.out.println(ds.numDistinctItr("abbbii", "abbi"));
 
 	}
 
