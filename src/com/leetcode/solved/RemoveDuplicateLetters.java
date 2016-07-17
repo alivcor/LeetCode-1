4e672b5fb4194192b646134a6452e7c1134a6921
AmanRaj <amankaraj@gmail.com>
2016-07-16 19:22:26 -0700

    https://leetcode.com/problems/remove-duplicate-letters/

--- src/com/leetcode/solved/RemoveDuplicateLetters.java
+++ src/com/leetcode/solved/RemoveDuplicateLetters.java
@@ -7,15 +7,15 @@ public class RemoveDuplicateLetters {
 		int count[] = new int[26];
 		for(int i = 0; i < s.length(); i++) count[s.charAt(i) - 'a']++;
 		int minValPos = 0;
-		for(int i = 1; i < s.length(); i++){
+		for(int i = 0; i < s.length(); i++){
 			if(s.charAt(i) < s.charAt(minValPos)) minValPos = i;
 			if(--count[s.charAt(i) - 'a'] == 0)break;
 		}
-		return (s.isEmpty()) ? "" : s.charAt(minValPos) + removeDuplicateLetters(s.replaceAll("" + s.charAt(minValPos), ""));
+		return (s.isEmpty()) ? "" : s.charAt(minValPos) + removeDuplicateLetters(s.substring(minValPos+1).replaceAll("" + s.charAt(minValPos), ""));
     }
 	
 	public static void main(String[] args) {
-		System.out.println(removeDuplicateLetters("abaab"));
+		System.out.println(removeDuplicateLetters("cbacdcbc"));
 	}
 
 }
