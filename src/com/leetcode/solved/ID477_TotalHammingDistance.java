72f71c4cc7fe520d66ac50c7e0d663dadb5e278b
AmanRaj <amankaraj@gmail.com>
2017-01-01 10:29:25 +0530

    https://leetcode.com/problems/total-hamming-distance/

--- src/com/leetcode/solved/ID477_TotalHammingDistance.java
+++ src/com/leetcode/solved/ID477_TotalHammingDistance.java
@@ -12,19 +12,36 @@ public class ID477_TotalHammingDistance {
     Total hamming distance considering only this position would be = (set_i)*(unset_i)
     Aggregate all of them.
     */
-	public int totalHammingDistance(int[] nums) {
+//	public int totalHammingDistance(int[] nums) {
+//        if(nums == null || nums.length <= 1) return 0;
+//        int val = 0;
+//        int[] setBits = new int[32];
+//        for(int i = 0; i < nums.length; i++){
+//            for(int j = 0; j < 32; j++){
+//                setBits[j] += ((nums[i]&(1<<j)) == 0) ? 0 : 1; 
+//            }
+//        }
+//        
+//        for(int i = 0; i < 32; i++){
+//            val += (nums.length - setBits[i])*setBits[i];
+//        }
+//        return val;
+//        
+//    }
+	
+	//constant space solution
+    public int totalHammingDistance(int[] nums) {
         if(nums == null || nums.length <= 1) return 0;
+        
         int val = 0;
-        int[] setBits = new int[32];
-        for(int i = 0; i < nums.length; i++){
-            for(int j = 0; j < 32; j++){
-                setBits[j] += ((nums[i]&(1<<j)) != 0) ? 1 : 0; 
+        for(int i = 0; i < 32; i++){
+            int bitC = 0;
+            for(int j = 0; j < nums.length; j++){
+                bitC += (nums[j]>>i)&1;
             }
+            val += bitC*(nums.length - bitC);
         }
         
-        for(int i = 0; i < 32; i++){
-            val += (nums.length - setBits[i])*setBits[i];
-        }
         return val;
         
     }
